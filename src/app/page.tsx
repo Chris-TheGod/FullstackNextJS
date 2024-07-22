03b2b47f5cfafcb8cdcbd856e9ae56d349bb032a
Kristjan Lääne <xonproject@gmail.com>
2024-07-22 17:33:16 +0300

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className='text-center pt-32 px-5'>
+      <h1 className='text-4xl md:text-5xl font-bold mb-5 '>
+        Welcome to my blog
+      </h1>
+      <p className='max-w-[750px] mx-auto leading-8'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+        corrupti voluptates quaerat a optio omnis. Vero exercitationem illo
+        neque optio magnam voluptate pariatur nulla laboriosam, iusto
+        repudiandae inventore quisquam veniam.
+      </p>
+    </main>
+  );
 }
