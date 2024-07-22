d1bcc6d84c0a28049e20803b2ef11d98fa7d26cf
Kristjan Lääne <xonproject@gmail.com>
2024-07-22 18:09:32 +0300

    link, routes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
+const navLinks = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/posts',
+    label: 'Posts',
+  },
+];
 
 export default function Header() {
   return (
@@ -10,6 +22,16 @@ export default function Header() {
         width='35'
         height='35'
       />
+
+      <nav>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
