47c567833987653aad5782d52096d9c16d28796e
Kristjan Lääne <xonproject@gmail.com>
2024-07-28 12:12:21 +0300

    prisma

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,5 +3,4 @@
 export async function createPost(formData: FormData) {
   const title = formData.get('title') as string;
   const body = formData.get('body') as string;
-  console.log(title, body);
 }
